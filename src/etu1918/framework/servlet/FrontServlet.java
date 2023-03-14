8d21d75bf1514096ffd61ecc788ee0431cc2ddf9
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2023-03-14 05:09:38 +0300

    c

--- src/etu1918/framework/servlet/FrontServlet.java
+++ src/etu1918/framework/servlet/FrontServlet.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FrontServlet extends HttpServlet {
 
     HashMap<String, Mapping> MappingUrls;
-    
+
 
     public void ProcessRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/plain");
